7466e8190127db5420f26ebd0e129cfcd210007b
yosselinz06 <yosselinzavala14@gmail.com>
2026-03-21 20:19:56 -0500

    Refactor backward A* algorithm with grid reset
    Refactor backward A* algorithm to include grid reset and adjust heapq usage for tie-breaking.

--- backward_astar.py
+++ backward_astar.py
@@ -1,10 +1,6 @@
 import heapq
 
 # Colors
-WHITE = (255,255,255)
-BLACK = (0,0,0)
-BLUE  = (0,0,255)
-GREEN = (0,255,0)
 RED   = (255,0,0)
 GOLD  = (255,215,0)
 
@@ -12,7 +8,7 @@ GOLD  = (255,215,0)
 def h(a,b):
     return abs(a.row - b.row) + abs(a.col - b.col)
 
-# Added Code here -------------------
+# reset grid
 def reset_grid(grid):
     for row in grid:
         for node in row:
@@ -20,12 +16,10 @@ def reset_grid(grid):
             node.f = float("inf")
             node.h=0
             node.parent=None
-# -----------------------------------
-
 
 # Path reconstruction
 def reconstruct_path(start,end):
-    current = end.parent # Added .parent
+    current = end.parent
     while current.parent and current != start:
         current.color = GOLD
         current = current.parent
@@ -35,16 +29,18 @@ def backward_a_star(grid,start,end):
     reset_grid(grid) #reset grid added
 
     open_set = []
+    closed_set = set()
     count = 0
 
     #search starts from end
     end.g = 0
     end.f = h(end,start)
-    heapq.heappush(open_set, (end.f, count, end))  # start from end node
-    closed_set = set()
+    heapq.heappush(open_set, (end.f, -end.g, count, end))  # start from end node
+    count += 1
+    
 
     while open_set:
-        current = heapq.heappop(open_set)[2]
+        current = heapq.heappop(open_set)[3]
 
         if current in closed_set: #skip nodes that are in closed set
             continue
@@ -64,11 +60,10 @@ def backward_a_star(grid,start,end):
             if temp_g < neighbor.g:
                 neighbor.parent = current
                 neighbor.g = temp_g
-                neighbor.f = temp_g + h(neighbor,start)
+                neighbor.f = neighbor.g + h(neighbor,start)
 
+                heapq.heappush(open_set,(neighbor.f, -neighbor.g, count, neighbor))
                 count += 1
-                heapq.heappush(open_set,(neighbor.f, count, neighbor))
-                # for tie breaking issue
 
                 if neighbor != end and neighbor != start:
                     neighbor.color = RED
